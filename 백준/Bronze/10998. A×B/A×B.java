7a589e5fca03f36708cca2fd96156992f675428c
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-05-29 23:00:29 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int result = firstNumber * secondNumber;
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
