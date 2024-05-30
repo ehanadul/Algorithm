24f397cab91d9fb76dceac7aeafd06f83d263119
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-05-30 22:29:40 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String ID = scanner.next();
+
+        System.out.println(ID + "??!");
+        scanner.close();
+    }
+}
