83d7606b6848e879fa678a58c982aae60becc239
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-06-05 02:05:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 320 ms, Memory: 18172 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int a =1 ; a <= n ; a++){
+            for (int b = 1; b <= a; b++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
