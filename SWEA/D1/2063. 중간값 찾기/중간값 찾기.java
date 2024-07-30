872697b365699312fbaa01c258b14e4566e2a627
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-07-30 15:06:58 +0900

    [D1] Title: 중간값 찾기, Time: 138 ms, Memory: 26,948 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,31 +1,47 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.io.FileInputStream;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-			int N = sc.nextInt();
-			int[] arr = new int[N];  
-			
-					
-			for(int i=0; i<N;i++) {
-				arr[i] =sc.nextInt();
-			}
-			
-			Arrays.sort(arr);
-			int m = arr[arr.length/2];
-			
-		
-            System.out.println(m);
-            sc.close();
-
-	}
+    
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+          
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 	
+    
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+        int N = Integer.parseInt(br.readLine());
+        int[] scores = new int[N];
+        
+
+        String[] scoreString = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(scoreString[i]); 
+        }
+
+        selectionSort(scores);
+        
+ 
+        int median = scores[N / 2];
+  
+        System.out.println(median);
+        br.close();
+        
+	}
 }
