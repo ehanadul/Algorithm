427ec8e1003e9fb82182fdcd7ba1470414812f7c
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-07-30 11:27:47 +0900

    [D2] Title: 중간 평균값 구하기, Time: 138 ms, Memory: 20,300 KB -BaekjoonHub

---
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T=sc.nextInt();
+		
+		int []arr = new int[10];
+		
+		for(int test_case = 1; test_case <= T; test_case++){
+			
+			for(int i =0;i<arr.length;i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			Arrays.sort(arr);
+			int sum =0;
+			
+			for(int i =1;i<arr.length-1;i++) {
+				sum += arr[i];
+			}
+			
+			 int avg = (int) Math.round((double) sum / 8);
+			System.out.println("#" + test_case + " " + avg);	
+			
+		}sc.close();
+	}
+}
