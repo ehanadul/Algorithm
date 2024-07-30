f0134db6df7412a4cfc947a1788ef565f3ee0e49
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-07-30 13:23:28 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 201 ms, Memory: 36,196 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,39 +1,37 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-	
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
-			int testNumber = sc.nextInt();
-			int[] frequency = new int[101];
-			
-			
-			for(int i=0;i<1000;i++) {
-				frequency[sc.nextInt()]++;
-			}
-			
-			
-			int mode = -1;
-			int maxFrequency = 0;
-			
-			
-			for(int score =0; score <= 100; score++) {
-				if(frequency[score] > maxFrequency || 
-				  (frequency[score] == maxFrequency && score > mode)) {
-					maxFrequency = frequency[score]; 
-					mode = score; 
-				}
-				
-			}System.out.println("#" + testNumber + " " + mode);		
-		}sc.close();
-	}
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int testNumber = Integer.parseInt(br.readLine());
+            int[] frequency = new int[101]; 
+            
+            
+            String[] scores = br.readLine().split(" ");
+            for (int i = 0; i < 1000; i++) {
+                int score = Integer.parseInt(scores[i]);
+                frequency[score]++;
+            }
+            
+            int mode = 0; 
+            int maxFrequency = frequency[0]; 
+            
+
+            for (int score = 1; score <= 100; score++) {
+                if (frequency[score] > maxFrequency || 
+                    (frequency[score] == maxFrequency && score > mode)) {
+                    maxFrequency = frequency[score];
+                    mode = score;
+                }
+            }
+            
+       
+            System.out.println("#" + testNumber + " " + mode);    
+        }
+    }
 }
