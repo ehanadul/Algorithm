5dfc5430b8603ef9447fe581c3997d4ecce16b0e
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-08-01 15:33:13 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 178 ms, Memory: 31,008 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -10,44 +10,45 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for (int tc = 1; tc <= 10; tc++) {
-            int t = Integer.parseInt(br.readLine()); 
+            int t = Integer.parseInt(br.readLine());
             int[][] arr = new int[SIZE][SIZE];
 
+          
             for (int i = 0; i < SIZE; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine()); 
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < SIZE; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken()); 
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            int max = maxSum(arr); 
-            System.out.println("#" + t + " " + max); 
+            int max = maxSum(arr);
+            System.out.printf("#%d %d%n", t, max);
         }
     }
 
     static int maxSum(int[][] arr) {
         int max = 0;
-
+        int diagonalSumA = 0;
+        int diagonalSumB = 0;
 
         for (int i = 0; i < SIZE; i++) {
             int rowSum = 0;
             int columnSum = 0;
+
             for (int j = 0; j < SIZE; j++) {
-                rowSum += arr[i][j];        // 행 합
+                rowSum += arr[i][j];        // 행 
                 columnSum += arr[j][i];     // 열 
             }
+
+   
             max = Math.max(max, Math.max(rowSum, columnSum));
+
+         
+            diagonalSumA += arr[i][i];              //대각선a
+            diagonalSumB += arr[i][SIZE - 1 - i];   //대각선b
         }
 
-        // 대각선
-        int diagonalSumA = 0;
-        int diagonalSumB = 0;
-        for (int i = 0; i < SIZE; i++) {
-            diagonalSumA += arr[i][i];          
-            diagonalSumB += arr[i][SIZE - 1 - i]; 
-        }
-
-    
+   
         max = Math.max(max, Math.max(diagonalSumA, diagonalSumB));
 
         return max;
