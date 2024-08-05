8f384e6b5ffd580d9a3afba1b578d44674b79a76
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-08-05 13:42:34 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 106 ms, Memory: 19,224 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,51 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Solution {
 
-	static int T = 10;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int tc =1; tc<=10; tc++) {
-			int lenght = Integer.parseInt(br.readLine());
-			String input = br.readLine();
-			
-			int isValid = check(input) ? 1 : 0 ;
-			System.out.println("#" + tc + " " + isValid);
-		}
-					
-	}				
-					
-	private static boolean check(String input){
-		Stack<Character> stack = new Stack<>();
-		
-		for(char bracket : input.toCharArray()) {
-			
-			if(bracket =='(' || bracket == '[' || bracket == '{' || bracket == '<') {
-				stack.push(bracket);
-			}
-			else if (bracket ==')' || bracket == ']' || bracket == '}' || bracket == '>') {
-				if(stack.isEmpty()) {
-					return false;
-				}
-				
-				char top = stack.pop();
-				
-				if (!isMatching(top,bracket)) {
-					return false;
-				}
-			}
-		}
-		return stack.isEmpty();
-	}
-	
-	private static boolean isMatching(char open, char close) {
-		return (open == '(' && close == ')') ||
-				(open == '[' && close == ']') ||
-				(open == '{' && close == '}') ||
-				(open == '<' && close == '>');
-	}					
+    static int T = 10;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int tc = 1; tc <= T; tc++) {
+            int length = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+
+            int isValid = check(input) ? 1 : 0;
+            System.out.println("#" + tc + " " + isValid);
+        }
+    }
+
+    private static boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        
+        // 열린 괄호와 닫힌 괄호의 매핑
+        Map<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put('(', ')');
+        bracketMap.put('[', ']');
+        bracketMap.put('{', '}');
+        bracketMap.put('<', '>');
+
+        for (char bracket : input.toCharArray()) {
+            if (bracketMap.containsKey(bracket)) {
+                stack.push(bracket); // 열린 괄호는 스택에 푸시
+            } else if (bracketMap.containsValue(bracket)) {
+                if (stack.isEmpty() || bracketMap.get(stack.pop()) != bracket) {
+                    return false; // 스택이 비어있거나 짝이 맞지 않으면 false
+                }
+            }
+        }
+        
+        return stack.isEmpty(); // 스택이 비어있으면 유효함
+    }
 }
