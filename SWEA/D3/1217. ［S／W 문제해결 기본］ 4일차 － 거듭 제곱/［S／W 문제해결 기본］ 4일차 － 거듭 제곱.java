faab51b30cd4163560fe2a2aa2e64dc3ff0977d5
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-08-06 11:38:45 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 177 ms, Memory: 19,960 KB -BaekjoonHub

---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		for (int tc = 1; tc <= 10; tc++) {
+			sc.nextLine(); 
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			sc.nextLine(); 
+
+			System.out.println("#" + tc + " " + pow(N, M));
+		}
+		sc.close();
+	}
+	
+	private static int pow(int N, int M) {
+		if(M == 0 || N == 1) {
+			return 1;
+		}
+		return N*pow(N, M-1);
+	}
+	
+}
