595d26a47923d61598cdc1e0237abd3fa777136a
ehanadul <156569727+ehanadul@users.noreply.github.com>
2024-08-08 14:43:09 +0900

    [D2] Title: 스도쿠 검증, Time: 159 ms, Memory: 24,076 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,56 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
- 
+import java.util.StringTokenizer;
+
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         
-        int[][] sudoku = new int[9][9];
-        int T = sc.nextInt(); 
-         
-        for (int test_case = 1; test_case <= T; test_case++) {
-             
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int[][] sudoku = new int[9][9];
             boolean isValid = true;
- 
+
             for (int i = 0; i < 9; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine().trim());
                 for (int j = 0; j < 9; j++) {
-                    sudoku[i][j] = sc.nextInt();
+                    sudoku[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-             
-            // 중복 검사
+
             Set<Integer>[] row = new HashSet[9];
             Set<Integer>[] col = new HashSet[9];
             Set<Integer>[] box = new HashSet[9];
-             
-            // 초기화
+
             for (int i = 0; i < 9; i++) {
                 row[i] = new HashSet<>();
                 col[i] = new HashSet<>();
                 box[i] = new HashSet<>();
             }
-             
-            outerLoop: for (int i = 0; i < 9; i++) {
+
+            out: for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
                     int num = sudoku[i][j];
-                     
-                    if (row[i].contains(num) || col[j].contains(num) || box[(i/3)*3 + j/3].contains(num)) {
+
+                    if (row[i].contains(num) || col[j].contains(num) || box[(i / 3) * 3 + j / 3].contains(num)) {
                         isValid = false;
-                        break outerLoop;
+                        break out;
                     }
-                     
+
                     row[i].add(num);
                     col[j].add(num);
-                    box[(i/3)*3 + j/3].add(num);
+                    box[(i / 3) * 3 + j / 3].add(num);
                 }
             }
- 
-            if (isValid) {
-                System.out.println("#" + test_case + " " + 1);
-            } else {
-                System.out.println("#" + test_case + " " + 0);
-            }
+
+            System.out.printf("#%d %d\n", tc, (isValid ? 1 : 0));
         }
     }
 }
