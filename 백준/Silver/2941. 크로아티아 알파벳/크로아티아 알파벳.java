695dc8e46810deb1724232e4061c7e1c670de9ad
ehanadul <156569727+ehanadul@users.noreply.github.com>
2025-11-26 11:02:11 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -3,18 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String a = br.readLine();        
-        String[] croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
-		int index = 0;
 
-		for (int i = 0; i < croatia.length; i++) {		
-            index = a.indexOf(croatia[i]);            
-			if (index >= 0) { 
-				a = a.replaceAll(croatia[i], "A");
-			}
-		}
-		System.out.println(a.length());
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = new String(br.readLine());
+        int i = 0;
+        int count =0;
+        while ( i < s.length()){
+                    if(i+2 < s.length() && s.substring(i,i+3).equals("dz=")){
+
+                        
+                        count++;
+                        i=i+3;
+                    }
+                    else if(i+1 < s.length()){
+                            if(s.substring(i,i+2).equals("c=")
+                            || s.substring(i,i+2).equals("c-")
+                            || s.substring(i,i+2).equals("d-")
+                            || s.substring(i,i+2).equals("lj")
+                            || s.substring(i,i+2).equals("s=")
+                            || s.substring(i,i+2).equals("nj")
+                            || s.substring(i,i+2).equals("z=")){
+
+                               
+                                count++;
+                                i=i+2;
+
+                            }else {
+                                count++;
+                                i++;
+                            }
+
+                    }else {
+                        count++;
+                        i++;
+                    }
+
+        }
+        System.out.println(count);
+
+    }
 }
